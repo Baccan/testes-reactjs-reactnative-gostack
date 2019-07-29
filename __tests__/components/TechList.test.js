b8392917f235af000d906ffbe401677dea612bcb
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2019-07-29 17:05:19 -0300

    Mock do useSelector

--- __tests__/components/TechList.test.js
+++ __tests__/components/TechList.test.js
@@ -1,41 +1,59 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 
 import TechList from '~/components/TechList';
 
+jest.mock('react-redux');
+
 describe('TechList component', () => {
-  beforeEach(() => {
-    localStorage.clear();
-  });
+  it('should render tech list', () => {
+    // const techs = useSelector(state => state.techs); =>> recebe um callback, executa-o e verifica o array
+    useSelector.mockImplementation(cb => cb({
+      techs: ['Node.js', 'ReactJS']
+    }));
 
-  it('should be able to add new tech', () => {
-    // getBytestId = <ul data-testid="tech-list"></ul> // por exemplo
-    const { getByText, getByTestId, getByLabelText } = render(<TechList />);
+    const { getByTestId, getByText, debug } = render(<TechList />);
 
-    // change = <input onChange={e => e.target.value} />
-    // getByLabelText = label com htmlFor="tech" e um input com id="tech"
-    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
-    fireEvent.submit(getByTestId('tech-form'));
-
-    // fireEvent.click(getByText('Adicionar'));
+    debug();
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
-    expect(getByLabelText('Tech')).toHaveValue('');
+    expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
   });
 
-  it('should store techs in storage', () => {
-    let { getByTestId, getByLabelText, getByText } = render(<TechList />);
+  // TESTE EM FORMULÁRIO
+  // beforeEach(() => {
+  //   localStorage.clear();
+  // });
 
-    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
-    fireEvent.submit(getByTestId('tech-form'));
+  // it('should be able to add new tech', () => {
+  //   // getBytestId = <ul data-testid="tech-list"></ul> // por exemplo
+  //   const { getByText, getByTestId, getByLabelText } = render(<TechList />);
+
+  //   // change = <input onChange={e => e.target.value} />
+  //   // getByLabelText = label com htmlFor="tech" e um input com id="tech"
+  //   fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+  //   fireEvent.submit(getByTestId('tech-form'));
+
+  //   // fireEvent.click(getByText('Adicionar'));
+
+  //   expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+  //   expect(getByLabelText('Tech')).toHaveValue('');
+  // });
+
+  // it('should store techs in storage', () => {
+  //   let { getByTestId, getByLabelText, getByText } = render(<TechList />);
+
+  //   fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+  //   fireEvent.submit(getByTestId('tech-form'));
     
-    // Limpa DOM para
-    cleanup();
+  //   // Limpa DOM para
+  //   cleanup();
 
-    // (desestrutuação) = parenteses por volta permite reutilizar a desestruturação após a declaração da variável
-    ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
+  //   // (desestrutuação) = parenteses por volta permite reutilizar a desestruturação após a declaração da variável
+  //   ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
 
-    expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']));
-    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
-  });
+  //   expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']));
+  //   expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+  // });
 });
