6e17cf887201d3bcbeef6f17a7ec360f090772c8
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-07-31 09:31:38 -0300

    Testes no React Native - Finalizados

--- react-native-testes/__tests__/TechList.test.js
+++ react-native-testes/__tests__/TechList.test.js
@@ -3,15 +3,15 @@ import React from 'react';
 import TechList from '~/components/TechList';
 
 import { render, fireEvent } from '@testing-library/react-native';
-import { escapeComponent } from 'uri-js';
 
 describe('TechList', () => {
   it('should be able to add new tech', () => {
     const { getByText, getByTestId } = render(<TechList />);
 
-    fireEvent.changeText(getAllByTestId('tech-input'), 'Node.js');
+    fireEvent.changeText(getByTestId('tech-input'), 'Node.js');
     fireEvent.press(getByText('Adicionar'));
 
     expect(getByText('Node.js')).toBeTruthy();
+    expect(getByTestId('tech-input')).toHaveProp('value', '');
   })
 })
--- react-native-testes/src/components/TechList.js
+++ react-native-testes/src/components/TechList.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, Text, FlatList } from 'react-native';
 
 // import { Container } from './styles';
 
 export default function TechList() {
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
   return (
-    <View />
+    <View>
+      <FlatList 
+        data={techs}
+        keyExtractor={tech => tech}
+        renderItem={({ item }) => <Text>{item}</Text>}
+      />
+
+      <TextInput testID="tech-input" value={newTech} onChangeText={setNewTech} />
+      <TouchableOpacity onPress={handleAdd}>
+        <Text>Adicionar</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
